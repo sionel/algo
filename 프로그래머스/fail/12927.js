128606a576964421a1540c13075ae16f455924b4
sionel <sadb0101@naver.com>
2023-05-28 13:20:38 +0900

    fail: 12927
    테스트케이스

--- 프로그래머스/fail/12927.js
+++ 프로그래머스/fail/12927.js
@@ -1,9 +1,8 @@
 function solution(n, works) {
-  let answer = 0;
-  
+  // 테스트케이스 3번과 효율 2번 이 틀리는 코드
   works.sort((a,b)=>b-a)
   let process = 0
-  let preIndex = 0
+  
   while(n--){
       if(works[process] === 0) return 0
       works[process]--
@@ -13,6 +12,31 @@ function solution(n, works) {
   }
   
   return works.reduce((fatigue,cur)=>fatigue+(cur**2),0);
+}
+
+function solution(n, works) {
+  // 테스트케이스 12번만 틀리는 코드
+  let obj = {}
+  works.forEach(e=>obj[e] = (obj[e] || 0 )+ 1 )
+  let max = Math.max(...works)
+  while(n){
+      if(obj[max] > n) {
+          obj[max] -= n
+          obj[max-1] = (obj[max-1] || 0) + n
+          n = 0
+      } else {
+          obj[max-1] = (obj[max-1] || 0) + obj[max]
+          n -= obj[max]
+          delete obj[max--]
+      }
+      if(obj[0]) return 0
+  }
+  
+  let answer = 0
+  for (const key in obj) {
+      answer += (Number(key)**2) * obj[key]
+  }
+  return answer
 }
 /* 
 9 8 8 7
