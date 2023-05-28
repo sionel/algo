0b8bf4c82b70de53d7652da08c2cf536939127b6
sionel <sadb0101@naver.com>
2023-05-28 12:32:30 +0900

    fail: 프로그래머스 12927 야근 지수

--- 백준/test.js
+++ 백준/test.js
@@ -1,27 +1,44 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+// process.stdin.resume();
+// process.stdin.setEncoding('utf8');
 
-let data = [];
-const reader = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-reader.on('line', (line) => {
-  data.push(line);
-  reader.close();
-});
-reader.on('close', () => {
-  console.log(solution(...data));
-  
-  process.exit();
-});
+// let data = [];
+// const reader = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+// reader.on('line', (line) => {
+//   data.push(...line.split(' '));
+//   reader.close();
+// });
+// reader.on('close', () => {
+//   console.log(solution(...data));
+//   process.exit();
+// });
 
-const solution = (str) => {
-  const length = str.length
-  
-  for(let i = 0 ; i < str.length/2 ; i++){
-    if(str[i] !== str[length-1-i]) return 0
+const solution = (str, num) => {
+  let pivot = 0
+  for(let i = 1 ; i < str.length ; i++){
+    if(str[pivot] === str[i]){
+      pivot++
+    }else if(pivot){
+      pivot = 0
+      i--
+    }
   }
-  
-  return 1
+  return str.length + (str.length - pivot) * (Number(num)-1)
 }
+
+console.log(solution("abababa", 2));
+// function makeTable(pattern) {
+//   const table = Array.from({ length: pattern.length }, () => 0);
+//   let j = 0;
+//   for (let i = 1; i < pattern.length; i++) {
+//     while (j > 0 && pattern[i] !== pattern[j]) j = table[j - 1];
+//     console.log(j);
+//     if (pattern[i] === pattern[j]) table[i] = ++j;
+//   }
+//   return table;
+// }
+
+// makeTable("ababaabacb")
+// console.log();
