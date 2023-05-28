9833de3b70b5724fe500ab727e29ffd95717bb12
sionel <sadb0101@naver.com>
2023-05-28 12:33:02 +0900

    fail: 프로그래머스 12927 야근 지수

---
+++ 프로그래머스/fail/12927.js
@@ -0,0 +1,17 @@
+function solution(n, works) {
+  let answer = 0;
+  
+  works.sort((a,b)=>b-a)
+  let process = 0
+  let preIndex = 0
+  while(n--){
+      if(works[process] === 0) return 0
+      works[process]--
+      if(process === works.length -1 || works[process] === works[process+1]) process =0
+      else if(works[process] < works[process+1] ) process++
+      // else if(process && works[process-1] > works[process]) process--
+  }
+  
+  return works.reduce((fatigue,cur)=>fatigue+(cur**2),0);
+}
+//7, [10, 5, 7], 75
