383c18fcb977206b17e838457d693348dc7fabc0
최가오 <44205245+sionel@users.noreply.github.com>
2023-05-31 14:26:06 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.42 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,38 @@
+function solution(str1, str2) {
+    var answer = 0;
+    const arr1 = [...str1.toLowerCase()]
+    const arr2 = [...str2.toLowerCase()]
+    
+    const obj1 = {}
+    const obj2 = {}
+    const allKeywords = {}
+    arr1.forEach((e,i,arr)=>{
+        if(arr[i+1]) {
+            const str = e+arr[i+1]
+            if(!(str.match(/[^a-z]/g)?.length)){
+                obj1[str] = (obj1[str] || 0) +1 
+                allKeywords[str] = 1
+            }
+                
+        }
+    })
+    arr2.forEach((e,i,arr)=>{
+        if(arr[i+1]) {
+            const str = e+arr[i+1]
+            if(!(str.match(/[^a-z]/g)?.length)){
+                obj2[str] = (obj2[str] || 0) +1 
+                allKeywords[str] = 1
+            }
+        }
+    })
+
+    let intersection = 0
+    let combination = 0
+    Object.keys(allKeywords).forEach(e=>{
+        combination += Math.max((obj1[e] || 0) , (obj2[e] || 0) )
+        intersection += Math.min((obj1[e] || 0) , (obj2[e] || 0) )
+    })
+    
+    
+    return combination ? Number.parseInt(intersection/combination * 65536) : 65536
+}
