45b6f5ee0ee3eb2f3394bd7583eb3b3650d45193
최가오 <44205245+sionel@users.noreply.github.com>
2023-05-31 14:28:04 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -5,13 +5,13 @@ function solution(str1, str2) {
     
     const obj1 = {}
     const obj2 = {}
-    const allKeywords = {}
+    const allKeywords = new Set()
     arr1.forEach((e,i,arr)=>{
         if(arr[i+1]) {
             const str = e+arr[i+1]
             if(!(str.match(/[^a-z]/g)?.length)){
                 obj1[str] = (obj1[str] || 0) +1 
-                allKeywords[str] = 1
+                allKeywords.add(str)
             }
                 
         }
@@ -21,14 +21,14 @@ function solution(str1, str2) {
             const str = e+arr[i+1]
             if(!(str.match(/[^a-z]/g)?.length)){
                 obj2[str] = (obj2[str] || 0) +1 
-                allKeywords[str] = 1
+                allKeywords.add(str)
             }
         }
     })
 
     let intersection = 0
     let combination = 0
-    Object.keys(allKeywords).forEach(e=>{
+    allKeywords.forEach(e=>{
         combination += Math.max((obj1[e] || 0) , (obj2[e] || 0) )
         intersection += Math.min((obj1[e] || 0) , (obj2[e] || 0) )
     })
