b3a2c63d6077ea2cf2cf25b21be93714ac887f02
최가오 <44205245+sionel@users.noreply.github.com>
2023-07-08 22:54:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 532 ms, Memory: 55816 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -1,20 +1,12 @@
-const fs = require('fs');
-const input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require("fs").readFileSync(filePath).toString().trim().split('\n')
 
-const N = Number(input.shift());
-const xy = input.filter((v, i) => i >= 0).map((v) => v.split(' '));
-
-
+const arr = input.slice(1).map(e => e.replace('\r', '').split(' '))
 let answer = '';
-xy.sort((a, b) => {
-  if (a[0] === b[0])
-    return a[1] - b[1];
-  else
-    return a[0] - b[0];
-})
-  .forEach(arr => {
-    answer += `${arr[0]} ${arr[1]}\n`
-  });
 
-
+arr.sort((a, b) => {
+  return a[0] === b[0] ? a[1] - b[1]:  a[0] - b[0]
+}).forEach(arr => {
+  answer += `${arr[0]} ${arr[1]}\n`
+});
 console.log(answer);
