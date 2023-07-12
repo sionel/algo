32b483d16f321efbaa7bc370b0581468099bc6fa
최가오 <44205245+sionel@users.noreply.github.com>
2023-07-12 12:02:03 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.js
+++ 백준/Silver/7568. 덩치/덩치.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().split('\n')
+const input = fs.readFileSync(filePath).toString().trim().split('\n')
 const number = input.shift()
 
 const arr = input.map(e => e.split(' ').map(Number))
 let result = []
-for (let i = 0; i < number; i++) {
+for (let i = 0; i < arr.length; i++) {
   let rank = 1
-  for (let j = 0; j < number; j++) {
+  for (let j = 0; j < arr.length; j++) {
     if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1])
       rank++
   }
