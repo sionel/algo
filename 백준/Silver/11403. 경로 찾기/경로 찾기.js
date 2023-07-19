c958d7cff0bd8bb3f7c264c1cf8e3e97a972c6eb
최가오 <44205245+sionel@users.noreply.github.com>
2023-07-19 13:37:53 +0900

    [Silver I] Title: 경로 찾기, Time: 184 ms, Memory: 9996 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.js
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().split('\n');
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const n = Number(input.shift())
 const board = input.map(e => e.split(' ').map(Number))
@@ -16,6 +16,7 @@ for (let k = 0; k < n; k++) {
   // 출력예시에 맞게 출력
 
 }
-for (let i = 0; i < n; i++) {
-  console.log(board[i].join(' '));
-}
+// for (let i = 0; i < n; i++) {
+//   console.log(board[i].join(' '));
+// }
+console.log(board.map(e => e.join(' ')).join('\n'));
