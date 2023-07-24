47cdc3788431b4d32349f13169672ab767944580
sionel <sadb0101@naver.com>
2023-07-24 14:33:49 +0900

    Feat: 백준 5373 푸는중

--- 백준/jstest.js
+++ 백준/jstest.js
@@ -1,19 +1,77 @@
 const fs = require("fs");
 
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
-const [number, arr] = input
-let r = 31n
-let M = 1234567891n
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = (() => {
+  const stdin = fs.readFileSync(filePath).toString().split('\n');
+  let ln = 0;
+  return () => stdin[ln++];
+})();
 
-let result = 0n
-let pow = 1n
+const num = Number(input())
+const cube =
+  [
+    [
+      ['w', 'w', 'w'],
+      ['w', 'w', 'w'],
+      ['w', 'w', 'w']
+    ],
+    [
+      ['r', 'r', 'r'],
+      ['r', 'r', 'r'],
+      ['r', 'r', 'r']
+    ],
+    [
+      ['y', 'y', 'y'],
+      ['y', 'y', 'y'],
+      ['y', 'y', 'y']
+    ],
+    [
+      ['o', 'o', 'o'],
+      ['o', 'o', 'o'],
+      ['o', 'o', 'o']
+    ],
+    [
+      ['g', 'g', 'g'],
+      ['g', 'g', 'g'],
+      ['g', 'g', 'g']
+    ],
+    [
+      ['b', 'b', 'b'],
+      ['b', 'b', 'b'],
+      ['b', 'b', 'b']
+    ]
+  ]
 
-for (let i = 0; i < arr.length; i++) {
-  result += BigInt(arr[i].charCodeAt(0) - 96) * pow;
-  pow *= r;
+for (let i = 0; i < num; i++) {
+  const cases = Number(input())
+  const queries = input().replace('\r', '').split(' ')
+  for (let j = 0; j < cases; j++) {
+    const query = queries[j]
+    switch (query) {
+      case 'U+':
+        break;
+      case 'D+':
+        break;
+      case 'F+':
+        break;
+      case 'B+':
+        break;
+      case 'L+':
+        break;
+      case 'R+':
+        break;
+      case 'U-':
+        break;
+      case 'D-':
+        break;
+      case 'F-':
+        break;
+      case 'B-':
+        break;
+      case 'L-':
+        break;
+      case 'R-':
+        break;
+    }
+  }
 }
-
-if(result >= M) result %= M;
-console.log(Number(result));
-//https://vscode.dev/profile/github/7ac0659b4dc22231adeaba18649b16cc
