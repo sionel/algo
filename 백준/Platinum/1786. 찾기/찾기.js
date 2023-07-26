4299e54bf894b52de70aac786f2704c66a74311f
최가오 <44205245+sionel@users.noreply.github.com>
2023-07-26 15:58:54 +0900

    [Platinum V] Title: 찾기, Time: 436 ms, Memory: 78616 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().replaceAll('\r', '').split('\n');
+
+const [T, P] = input
+
+const makeTable = (pattern) => {
+  const table = Array.from({ length: pattern.length }, () => 0);
+  let j = 0;
+  for (let i = 1; i < pattern.length; i++) {
+    while (j > 0 && pattern[i] !== pattern[j]) j = table[j - 1];
+    if (pattern[i] === pattern[j]) table[i] = ++j;
+  }
+  return table
+}
+
+const solution = (T, P) => {
+  const table = makeTable(P)
+  let position = []
+  let j = 0
+  for (let i = 0; i < T.length; i++) {
+    while (j > 0 && T[i] !== P[j]) j = table[j - 1]
+    if (T[i] === P[j]) {
+      if (j === P.length - 1) {
+        position.push(i - j + 1)
+        j = table[j]
+      } else {
+        j++
+      }
+    }
+  }
+  console.log(position.length);
+  console.log(position.join(' '));
+}
+
+solution(T, P)
