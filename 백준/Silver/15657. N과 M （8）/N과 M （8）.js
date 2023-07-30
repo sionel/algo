b0cea11c42edb09ad18f64f93c2772f88cdf0b1c
최가오 <44205245+sionel@users.noreply.github.com>
2023-07-30 22:23:06 +0900

    [Silver III] Title: N과 M (8), Time: 584 ms, Memory: 13476 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number)
+
+const arr = input[1].split(' ').map(Number)
+arr.sort((a, b) => a - b)
+const result = []
+const dp = (n, m) => {
+  if (m === M) {
+    console.log(result.join(' '));
+  }
+  else {
+    for (let i = n; i < arr.length; i++) {
+      result.push(arr[i])
+      dp(i, m + 1)
+      result.pop()
+    }
+  }
+}
+
+dp(0, 0)
