70898d04d7ab25c10a87fa1a29ca4dd7822650da
최가오 <44205245+sionel@users.noreply.github.com>
2023-08-10 22:26:35 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let str = input[0]
+
+
+
+console.log(`${str}??!`);
