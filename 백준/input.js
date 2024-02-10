3806cefd9324e6dee5a147c56ca98787c393cf64
최가오 <44205245+sionel@users.noreply.github.com>
2024-02-11 08:27:38 +0900

    입력 포맷 하나 추가

--- 백준/input.js
+++ 백준/input.js
@@ -1,8 +1,24 @@
 const fs = require("fs");
 
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = (() => {
-  const stdin = fs.readFileSync(filePath).toString().split('\n');
+  const stdin = fs.readFileSync(filePath).toString().split("\n");
   let ln = 0;
   return () => stdin[ln++];
 })();
+
+const reader = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+reader.on("line", (line) => {
+  if (count === null) count = Number(line);
+  else {
+    lines.push(line.split(" ").map((e) => Number(e)));
+    if (--count === 0) reader.close();
+  }
+});
+reader.on("close", () => {
+  solution(lines);
+  process.exit();
+});
