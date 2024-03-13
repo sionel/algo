a1f4da9e18f3521d2701cb76ba003793e2841003
최가오 <44205245+sionel@users.noreply.github.com>
2024-03-13 22:25:26 +0900

    [Bronze II] Title: カードゲーム 2 (Card Game 2), Time: 292 ms, Memory: 39612 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/31618. カードゲーム 2 （Card Game 2）/カードゲーム 2 （Card Game 2）.js
+++ 백준/Bronze/31618. カードゲーム 2 （Card Game 2）/カードゲーム 2 （Card Game 2）.js
@@ -3,16 +3,16 @@ const fs = require("fs");
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n')
 
-const n = Number(input[0])
-const arr = [...new Set(input[1].split(' ').map(Number))].sort((a, b) => a - b)
+// const n = Number(input[0])
+const arr = new Set(input[1].split(' ').map(Number))
+const map = Array(200001).fill(false).map((_, i) => arr.has(i))
 let result = "No"
-for(let i = 0; i < arr.length-2; i++) {
-  const temp = new Set(arr)
-  temp.add(arr[i]+3)
-  temp.add(arr[i]+6)  
-  if(temp.size === arr.length) {
+for(let i = 0 ; i < map.length; i++){
+  if(map[i] && map[i+3] && map[i+6]){
     result = "Yes"
     break
+  }else if(map[i] && map[i+3]){
+    map[i+3] = false
   }
 }
 console.log(result);
