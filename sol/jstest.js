8994c147774e9c52ed7c6f583fcc67d7f93dfd15
최가오 <44205245+sionel@users.noreply.github.com>
2024-05-03 10:54:48 +0900

    sol

--- sol/jstest.js
+++ sol/jstest.js
@@ -1,51 +1,26 @@
 const fs = require("fs");
 
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .replace("\r", "")
+  .split("\n");
 
-const input = fs.readFileSync(filePath).toString().replaceAll('\r', '').split('\n')
-
-const sudoku = input.map((row) => row.split("").map(Number))
-
-
-const check = (i,j) => {
-  const checkList = Array.from({ length: 10 }, () => 0);
-  for (let k = 0; k < 9; k++) {
-    checkList[sudoku[i][k]] = 1;
-    checkList[sudoku[k][j]] = 1;
+const arr = input.map(Number);
+const result = [];
+const hanoi4 = [0, 1n];
+let n = 2n;
+while (true) {
+  for (let i = 1; i <= n; i++) {
+    hanoi4.push(hanoi4[hanoi4.length - 1] + 2n ** (n - 1n));
+    if (hanoi4.length === 1002) break;
   }
-  const x = Math.floor(i / 3) * 3;
-  const y = Math.floor(j / 3) * 3;
-  for (let a = x; a < x + 3; a++) {
-    for (let b = y; b < y + 3; b++) {
-      checkList[sudoku[a][b]] = 1;
-    }
-  }
-  return checkList;
+  if (hanoi4.length === 1002) break;
+  n++;
 }
-const conjecture = (x, y) => {
-  for (let i = x; i < 9; i++) {
-    for (let j = y; j < 9; j++) {
-      if (!sudoku[i][j]) {
-        const checkList = check(i, j);
-        for (let num = 1; num <= 9; num++) {
-          if (!checkList[num]) {
-            sudoku[i][j] = num;
-            if (conjecture(i, j)) {
-              return true;
-            }
-            sudoku[i][j] = 0;
-          }
-        }
-        return false
-      }
-      if(i === 8 && j === 8) {
-        return true
-      }
-    }
-    y = 0
-  }
-};
-
-conjecture(0, 0);
-
-console.log(sudoku.map((row) => row.join("")).join("\n"));
+for (let i = 0; i < arr.length; i++) {
+  result.push(`Case ${i + 1}: ${hanoi4[arr[i]]}`);
+}
+console.log(result.join("\n"));
