c271f3c00a80cab9a46b02148fab4c4af9673828
최가오 <44205245+sionel@users.noreply.github.com>
2024-05-03 10:52:13 +0900

    [Gold II] Title: 하노이의 네 탑, Time: 120 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Gold/9942. 하노이의 네 탑/하노이의 네 탑.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .replace("\r", "")
+  .split("\n");
+
+const arr = input.map(Number);
+const result = [];
+const hanoi4 = [0, 1n];
+let n = 2n;
+while (true) {
+  for (let i = 1; i <= n; i++) {
+    hanoi4.push(hanoi4[hanoi4.length - 1] + 2n ** (n - 1n));
+    if (hanoi4.length === 1002) break;
+  }
+  if (hanoi4.length === 1002) break;
+  n++;
+}
+for (let i = 0; i < arr.length; i++) {
+  result.push(`Case ${i + 1}: ${hanoi4[arr[i]]}`);
+}
+console.log(result.join("\n"));
