c4bc72b1e8d60f40276264fef1fa62a98c213259
최은우 <44205245+sionel@users.noreply.github.com>
2024-06-06 20:13:35 +0900

    [Gold IV] Title: 호텔, Time: 180 ms, Memory: 11832 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.js
+++ 백준/Gold/1106. 호텔/호텔.js
@@ -1,43 +1,21 @@
 const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./dev/stdin";
+const inputs = fs.readFileSync(filePath).toString().replaceAll("\r", "").trim().split("\n");
 
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .replaceAll("\r", "")
-  .split("\n");
+const [C, N] = inputs[0].split(" ").map(Number);
+const arr = inputs.slice(1).map((str) => str.split(" ").map(Number));
+const MAX = 1101;
 
-let [c, m] = input[0].split(" ").map(Number);
-const cityByCost = {};
-const dp = Array(2001).fill(Infinity);
-for (let i = 1; i <= m; i++) {
-  const [city, cost] = input[i].split(" ").map(Number);
-  dp[cost] = Math.min(dp[cost], city);
+const dp = Array(MAX).fill(Infinity);
+dp[0] = 0;
 
-  if (cityByCost[cost]) {
-    cityByCost[cost] = Math.min(cityByCost[cost], city);
-  } else {
-    cityByCost[cost] = city;
+for (let i = 1; i < dp.length; i++) {
+  let min = Infinity;
+  for (const [cost, cnt] of arr) {
+    const prevIdx = i - cnt;
+    if (prevIdx >= 0) min = Math.min(min, dp[prevIdx] + cost);
   }
+  dp[i] = min;
 }
-
-for (let i = 1; i <= 2001; i++) {
-  if (dp[i] !== Infinity) {
-    for (const cost in cityByCost) {
-      const city = Number(cityByCost[cost]);
-      dp[i + Number(cost)] = Math.min(dp[i + Number(cost)], dp[i] + city);
-    }
-  }
-}
-
-let min = Infinity;
-for (let i = 2001; i >= 1; i--) {
-  if (min > dp[i]) {
-    min = dp[i];
-  } else {
-    dp[i] = min;
-  }
-}
-
-console.log(dp[c]);
+const greaterThanC = arr.filter(([, cnt]) => cnt > 1000).map(([cost]) => cost);
+console.log(Math.min(...dp.slice(C), ...greaterThanC));
