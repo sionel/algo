0e7bf31556a6ce29f55f4200eeaaabe475efa8e6
최은우 <44205245+sionel@users.noreply.github.com>
2024-08-08 23:59:07 +0900

    [Gold V] Title: 시간 관리, Time: 120 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Gold/1263. 시간 관리/시간 관리.js
+++ 백준/Gold/1263. 시간 관리/시간 관리.js
@@ -1,4 +1,3 @@
-
 const fs = require("fs");
 
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./dev/stdin";
@@ -7,16 +6,15 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const n = Number(input[0]);
 const arr = [];
 for (let i = 1; i <= n; i++) {
-    const [a, b] = input[i].split(" ").map(Number);
-    arr.push([b, a]);
+    arr.push(input[i].split(" ").map(Number));
 }
-arr.sort((x, y) => y[0] - x[0]);
-let ans = arr[0][0] - arr[0][1];
+arr.sort((x, y) => y[1] - x[1]);
+let ans = arr[0][1] - arr[0][0];
 for (let i = 1; i < n; i++) {
-    if (ans > arr[i][0]) {
-        ans = arr[i][0] - arr[i][1];
+    if (ans > arr[i][1]) {
+        ans = arr[i][1] - arr[i][0];
     } else {
-        ans -= arr[i][1];
+        ans -= arr[i][0];
     }
 }
 console.log(ans >= 0 ? ans : -1);
